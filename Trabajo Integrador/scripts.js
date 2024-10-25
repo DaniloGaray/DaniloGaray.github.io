f578f2625780d10faec263e8d13ed1e31eb4dca0
DaniloGaray <141675715+DaniloGaray@users.noreply.github.com>
2024-10-25 19:04:48 -0300

    reservas, imagenes y validaciones

--- Trabajo Integrador/scripts.js
+++ Trabajo Integrador/scripts.js
@@ -56,8 +56,7 @@ function validar(){
 
   let validacionNombre =/^[A-Za-zÁÉÍÓÚáéíóúñÑ\s]{3,}$/;  
   let validacionDireccion = /^[A-Za-z0-9ÁÉÍÓÚáéíóúñÑ\s,'-]{5,100}$/;
-  let validacionEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
-;
+  let validacionEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   let validacionCelular = /^[0-9]{7,15}$/;
 
   if (!validacionNombre.test(nombre.value)) {
@@ -115,4 +114,86 @@ function limpiar(event){
   document.getElementById('result').style.display = 'none';
 
   
+}
+function validarReserva(){
+
+  let ban = false;
+
+  let nom = document.getElementById('nombreA');
+  let fecha = document.getElementById('fecha');
+  let personas = document.getElementById('personas');
+  let correo = document.getElementById('correo');
+  let tel = document.getElementById('tel');
+
+  let validacionNombreA =/^[A-Za-zÁÉÍÓÚáéíóúñÑ\s]{3,}$/; 
+  let validacionCorreo = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  let validacionCelular = /^[0-9]{7,15}$/;
+
+  
+
+  if (!validacionNombreA.test(nom.value)) {
+    document.getElementById('errorNombreA').style.display = 'block';
+    document.getElementById('nombreA').style.border = '1px solid red';
+    ban = true;
+  }
+  let hoy = new Date();
+  let fechaSeleccionada = new Date(fecha.value);
+  hoy.setHours(0,0,0,0)
+
+  if (fechaSeleccionada < hoy) {
+    document.getElementById('errorFecha').style.display = 'block';
+    document.getElementById('fecha').style.border = '1px solid red';
+    ban = true;
+  }
+  if (personas.value < 1 || personas.value > 20 || personas.value === "") {
+    document.getElementById('errorPersonas').style.display = 'block';
+    document.getElementById('personas').style.border = '1px solid red';
+    ban = true;
+  }
+
+  if (!validacionCorreo.test(correo.value)) {
+    document.getElementById('errorCorreo').style.display = 'block';                
+    document.getElementById('correo').style.border = '1px solid red';
+    ban = true;
+  }
+
+  if (!validacionCelular.test(tel.value)) {
+    document.getElementById('errorTel').style.display = 'block';
+    document.getElementById('tel').style.border = '1px solid red';
+    ban = true;
+  }
+
+  if (ban === false) {
+    let result = document.getElementById('resultado');
+    let texto = document.createElement('h2');
+    texto.innerHTML = 'Su reserva se realizo con exito para el dia '+fecha.value+', a nombre de '+nom.value+', para '+personas.value+' personas!';
+    result.appendChild(texto);
+    result.style.display = 'block';
+
+    nombreA.value="";
+    fecha.value="";
+    personas.value="";
+    correo.value="";
+    tel.value="";
+  }
+  return false;
+
+}
+function limpiarR(event){
+  document.getElementById('errorNombreA').style.display = 'none';
+  document.getElementById('nombreA').style.border = ' 1px solid #ccc';
+
+  document.getElementById('errorFecha').style.display = 'none';
+  document.getElementById('fecha').style.border = ' 1px solid #ccc';
+
+  document.getElementById('errorPersonas').style.display = 'none';
+  document.getElementById('personas').style.border = ' 1px solid #ccc';
+
+  document.getElementById('errorCorreo').style.display = 'none';
+  document.getElementById('correo').style.border = '1px solid #ccc';
+
+  document.getElementById('errorTel').style.display = 'none';
+  document.getElementById('tel').style.border = '1px solid #ccc';
+
+  document.getElementById('resultado').style.display = 'none';
 }
